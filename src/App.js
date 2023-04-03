b80ec57a3c6a3ecfbcc97568cedb0734b21fdf40
Kian <Kianmirzaie12114@gmail.com>
2023-04-03 14:06:02 +0330

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div id='wrapper'>
       <Section name='SEDANS' desc='Choose a sedan for its affordability & excellent fuel economy. Ideal for cruising in the city or on your next road trip.' id='sedan-btn' bg='hsl(31, 77%, 52%)' img='./images/icon-sedans.svg' />
       <Section name='SUVS' desc='Take an SUV for its spacious interior, power, & versatility. Perfect for your next family vacation & off-road adventures.' id='suv-btn' bg='hsl(184, 100%, 22%)' img='./images/icon-suvs.svg' />
-      <Section name='SEDANS' desc='Choose a sedan for its affordability & excellent fuel economy. Ideal for cruising in the city or on your next road trip.' id='lux-btn' bg='hsl(179, 100%, 13%)' img='./images/icon-luxury.svg' />
+      <Section name='LUXURY' desc='Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental & arrive in style.' id='lux-btn' bg='hsl(179, 100%, 13%)' img='./images/icon-luxury.svg' />
     </div>
   );
 }
